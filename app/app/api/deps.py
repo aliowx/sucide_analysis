3a8f53e1597a58c1d5d3daff51627a03aaa2ef4e
alialibakhshi <alialibakhshi9898@gmail.com>
2025-03-31 07:13:32 -0400

    feat: habdle basic and cookie loging is one deps

--- app/app/api/deps.py
+++ app/app/api/deps.py
@@ -41,5 +41,85 @@ async def get_redis()-> client.Redis:
         if await redis_client.ping():
             return redis_client
         raise redis.RedisError('ping error')
+    except Exception as e:
+        logger.error(f'Redis connection failed\n{e}')
+        raise e
+    
+async def get_user_id_from_cookie(
+    request: Request,
+    response: Response,
+    cache: client.Redis = Depends(get_redis)
+):
+    try:
+        access_token = request.cookies.get("Access-Token")
+        if not access_token:
+            raise exc.UnauthorizedException(
+                msg_code=utils.MessageCodes.access_token_not_found
+            )
+            
+        token = JWTHandler.decode(access_token)
+        if await cache.get(ACCESS_TOKEN_BLOCKLIST_KEY.format(token=access_token)):
+            raise exc.UnauthorizedException(
+                msg_code=utils.MessageCodes.expired_token
+            )
+        user_id = token.get('id')
+        
+        if token.get('sub')  != 'access' or not user_id:
+            raise exc.UnauthorizedException(
+                msg_code=utils.MessageCodes.inactive_user
+            )
+            
     except:
-        pass 
+        refresh_token = request.cookies.get("Refresh-Token")
+        if not refresh_token:
+            raise exc.UnauthorizedException(
+                msg_code=utils.MessageCodes.refresh_token_not_found
+            )
+            
+        refresh_token_data = JWTHandler.decode(refresh_token)
+        if (
+            await cache.get(REFRESH_TOKEN_BLOCKLIST_KEY.format(token=refresh_token))
+            or refresh_token_data.get('sub') != 'refresh'
+        ):
+            raise exc.UnauthorizedException(
+                msg_code=utils.MessageCodes.inactive_user
+            )
+            
+        user_id = refresh_token_data.get('id')
+        
+        token = JWTHandler.encode(payload={"sub": "access", "id": user_id})
+        
+        
+        response.set_cookie(
+            key="Access-Token",
+            value=token,
+            secure=True,
+            httponly=True,
+            samesite="strict" if not settings.DEBUG else "none",
+            expires=JWTHandler.token_expiration(token),
+            
+        ) 
+    
+    request.state.user_id = user_id
+    
+    return int(user_id)
+
+
+
+async def get_current_user_from_cookie(
+    db: AsyncSession = Depends(get_db_async),
+    current_user_id: int = Depends(get_user_id_from_cookie)
+)-> schemas.User:
+    current_user = await crud.user.get(db=db, id_=current_user_id)
+    
+    if not current_user:
+        raise exc.ForbiddenException(msg_code=utils.MessageCodes.invalid_token)
+    
+    
+    if not crud.user.is_active(current_user):
+        raise exc.ForbiddenException(
+            msg_code=utils.MessageCodes.inactive_user
+        )
+        
+        
+    return current_user
