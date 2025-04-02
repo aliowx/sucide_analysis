f20345f10fcfa2c99db3e506f009c287fcf5deb7
alialibakhshi <alialibakhshi9898@gmail.com>
2025-04-02 08:28:56 -0400

    refactor: refactor user services

--- app/app/api/api_v1/services/users.py
+++ app/app/api/api_v1/services/users.py
@@ -8,3 +8,29 @@ from app import (
     exceptions as exc,
     utils
 )
+
+async def read_user_by_id(
+    user_id: int,
+    current_user: models.User,
+    db:AsyncSession
+)->schemas.User:
+    user = await crud.user.get(db, id = user_id)
+    if not user:
+        raise exc.NotFoundException(
+            detail='User not found',
+            msg_code=utils.MessageCodes.not_found
+        )
+        
+    if user == current_user:
+        return user
+    
+    if not crud.user.is_superuser(
+        current_user
+    ):
+        raise exc.ForbiddenException(
+            detail="The user doesn't have enough privileges",
+            msg_code=utils.MessageCodes.bad_request
+        )
+        
+        
+    return user
